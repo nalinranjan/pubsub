02d5ba78b3567426917acaad10245745cb8fde69
Nalin Ranjan <nalin.ranjan@outlook.com>
2018-02-02 12:22:46 -0500

    Added publish, advertise messages; changed message format

--- src/edu/rit/CSCI652/impl/PubSubAgent.java
+++ src/edu/rit/CSCI652/impl/PubSubAgent.java
@@ -1,9 +1,7 @@
 package edu.rit.CSCI652.impl;
 
-import edu.rit.CSCI652.demo.Event;
 import edu.rit.CSCI652.demo.Publisher;
 import edu.rit.CSCI652.demo.Subscriber;
-import edu.rit.CSCI652.demo.Topic;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -60,7 +58,7 @@ public class PubSubAgent implements Publisher, Subscriber{
 	}
 
 	private void register() {
-		sendMessage("register " + listenPort);
+		sendMessage("register&" + listenPort);
 	}
 
 	private void startCli() {
@@ -76,6 +74,7 @@ public class PubSubAgent implements Publisher, Subscriber{
 			System.out.println("6. Advertise a new topic");
 			System.out.println("7. Publish an article");
 			System.out.println("8. View notifications");
+			System.out.println("9. Quit");
 			System.out.print("\n> ");
 
 			int selection = 0;
@@ -119,12 +118,13 @@ public class PubSubAgent implements Publisher, Subscriber{
 					publish();
 					break;
 				
+				case 9:
+					System.exit(0);
+
 				default:
 					break;
 			}
 		}
-
-		// stdIn.close();
 	}
 
 	public void listAllTopics() {
@@ -133,7 +133,7 @@ public class PubSubAgent implements Publisher, Subscriber{
 
 	@Override
 	public void subscribe() {
-		String message = "subscribe ";
+		String message = "subscribe&" + agentId + "&";
 		System.out.println("\nEnter topic ID: ");
 		try {
 			message += Integer.parseInt(stdIn.readLine());
@@ -147,7 +147,7 @@ public class PubSubAgent implements Publisher, Subscriber{
 
 	@Override
 	public void unsubscribe() {
-		String message = "unsubscribe ";
+		String message = "unsubscribe&" + agentId + "&";
 		System.out.println("\nEnter topic ID: ");
 		try {
 			message += Integer.parseInt(stdIn.readLine());
@@ -161,25 +161,37 @@ public class PubSubAgent implements Publisher, Subscriber{
 
 	@Override
 	public void unsubscribeAll() {
-		sendMessage("unsubscribeall");		
+		sendMessage("unsubscribeall&" + agentId);
 	}
 
 	@Override
 	public void listSubscribedTopics() {
-		sendMessage("subtopics");
+		sendMessage("subscribedtopics&" + agentId);
 	}
 
 	@Override
 	public void publish() {
-		// TODO Auto-generated method stub
-		
+		String message = "publish&";
+		try {
+			System.out.println("\nEnter title: ");
+			message += stdIn.readLine() + "&";
+			System.out.println("\nEnter topic ID: ");
+			message += stdIn.readLine() + "&";
+			System.out.println("\nEnter article contents: ");
+			message += stdIn.readLine(); 
+			sendMessage(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void advertise() {
-		String message = "advertise ";
-		System.out.println("\nEnter topic name: ");
+		String message = "advertise&";
 		try {
+			System.out.println("\nEnter topic name: ");
+			message += stdIn.readLine() + "&";
+			System.out.println("\nEnter keywords: ");
 			message += stdIn.readLine(); 
 			sendMessage(message);
 		} catch (IOException e) {
@@ -190,5 +202,4 @@ public class PubSubAgent implements Publisher, Subscriber{
 	public static void main(String[] args) {
 		PubSubAgent agent = new PubSubAgent();
 	}
-	
 }
